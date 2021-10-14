7a61396d9031251dc2435e621feb53e94683129d
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-10-14 10:38:57 +0700

    Video 5.Model, Collection & Controller

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,117 +32,9 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "Judul-post-pertama",
-            "author" => "Ardan Herdiansyah",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, ex quia. Accusamus ipsam maxime, 
-            quidem corrupti assumenda ut quaerat enim optio repellendus ex adipisci pariatur repellat quisquam earum 
-            voluptatum animi nihil quasi accusantium labore laboriosam libero dolores impedit minima! Quisquam reprehenderit 
-            uod molestiae ratione eos rerum reiciendis impedit fuga voluptatum expedita cupiditate perspiciatis accusamus, 
-            ipsa officia, ea maiores laborum autem ipsam minima? Porro mollitia suscipit eaque autem dolores blanditiis 
-            perferendis corrupti ipsam, cumque eveniet ad distinctio quod odio minima rem!"
-        ], 
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul-post-kedua",
-            "author" => "Riksa Kusumah",
-            "body" => "orem ipsum dolor sit amet consectetur adipisicing elit. Similique, natus non eum mollitia 
-            molestias laboriosam. Harum, totam autem! Facere inventore, corrupti temporibus, provident obcaecati 
-            blanditiis ad exercitationem maxime quisquam dicta veniam rem molestiae! Sunt, incidunt. Ab deserunt 
-            sint voluptatum expedita aliquam, adipisci et eius eligendi atque vel? Cupiditate saepe ullam atque 
-            facere officia facilis quisquam dolore qui beatae debitis! Quas quo dolor quibusdam, adipisci excepturi 
-            incidunt ratione esse inventore! Ipsa explicabo maiores velit sunt qui earum quasi eaque modi officia 
-            laboriosam, dignissimos numquam incidunt nihil ut facere impedit quia dicta ea debitis reprehenderit. 
-            Provident mollitia quisquam atque ducimus perspiciatis iure repellendus ab distinctio id sapiente incidunt 
-            quis in, voluptatum neque enim vero ipsa vel repellat unde, tempore quaerat nostrum? Blanditiis."
-        ],
-        [
-            "title" => "Judul Post Ketiga",
-            "slug" => "Judul-post-ketiga",
-            "author" => "Jagaddhita Arya K",
-            "body" => "orem ipsum dolor sit amet consectetur adipisicing elit. Similique, natus non eum mollitia 
-            molestias laboriosam. Harum, totam autem! Facere inventore, corrupti temporibus, provident obcaecati 
-            blanditiis ad exercitationem maxime quisquam dicta veniam rem molestiae! Sunt, incidunt. Ab deserunt 
-            sint voluptatum expedita aliquam, adipisci et eius eligendi atque vel? Cupiditate saepe ullam atque 
-            facere officia facilis quisquam dolore qui beatae debitis! Quas quo dolor quibusdam, adipisci excepturi 
-            incidunt ratione esse inventore! Ipsa explicabo maiores velit sunt qui earum quasi eaque modi officia 
-            laboriosam, dignissimos numquam incidunt nihil ut facere impedit quia dicta ea debitis reprehenderit. 
-            Provident mollitia quisquam atque ducimus perspiciatis iure repellendus ab distinctio id sapiente incidunt 
-            quis in, voluptatum neque enim vero ipsa vel repellat unde, tempore quaerat nostrum? Blanditiis. reiciendis impedit fuga voluptatum expedita cupiditate perspiciatis accusamus, 
-            ipsa officia, ea maiores laborum autem ipsam minima? Porro mollitia suscipit eaque autem dolores blanditiis 
-            perferendis corrupti ipsam, cumque eveniet ad distinctio quod odio minima rem!"
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
+Route::get('/blog', [PostController::class, 'index']);
 //halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "Judul-post-pertama",
-            "author" => "Ardan Herdiansyah",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, ex quia. Accusamus ipsam maxime, 
-            quidem corrupti assumenda ut quaerat enim optio repellendus ex adipisci pariatur repellat quisquam earum 
-            voluptatum animi nihil quasi accusantium labore laboriosam libero dolores impedit minima! Quisquam reprehenderit 
-            uod molestiae ratione eos rerum reiciendis impedit fuga voluptatum expedita cupiditate perspiciatis accusamus, 
-            ipsa officia, ea maiores laborum autem ipsam minima? Porro mollitia suscipit eaque autem dolores blanditiis 
-            perferendis corrupti ipsam, cumque eveniet ad distinctio quod odio minima rem!"
-        ], 
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul-post-kedua",
-            "author" => "Riksa Kusumah",
-            "body" => "orem ipsum dolor sit amet consectetur adipisicing elit. Similique, natus non eum mollitia 
-            molestias laboriosam. Harum, totam autem! Facere inventore, corrupti temporibus, provident obcaecati 
-            blanditiis ad exercitationem maxime quisquam dicta veniam rem molestiae! Sunt, incidunt. Ab deserunt 
-            sint voluptatum expedita aliquam, adipisci et eius eligendi atque vel? Cupiditate saepe ullam atque 
-            facere officia facilis quisquam dolore qui beatae debitis! Quas quo dolor quibusdam, adipisci excepturi 
-            incidunt ratione esse inventore! Ipsa explicabo maiores velit sunt qui earum quasi eaque modi officia 
-            laboriosam, dignissimos numquam incidunt nihil ut facere impedit quia dicta ea debitis reprehenderit. 
-            Provident mollitia quisquam atque ducimus perspiciatis iure repellendus ab distinctio id sapiente incidunt 
-            quis in, voluptatum neque enim vero ipsa vel repellat unde, tempore quaerat nostrum? Blanditiis."
-        ],
-        [
-            "title" => "Judul Post Ketiga",
-            "slug" => "Judul-post-ketiga",
-            "author" => "Jagaddhita Arya K",
-            "body" => "orem ipsum dolor sit amet consectetur adipisicing elit. Similique, natus non eum mollitia 
-            molestias laboriosam. Harum, totam autem! Facere inventore, corrupti temporibus, provident obcaecati 
-            blanditiis ad exercitationem maxime quisquam dicta veniam rem molestiae! Sunt, incidunt. Ab deserunt 
-            sint voluptatum expedita aliquam, adipisci et eius eligendi atque vel? Cupiditate saepe ullam atque 
-            facere officia facilis quisquam dolore qui beatae debitis! Quas quo dolor quibusdam, adipisci excepturi 
-            incidunt ratione esse inventore! Ipsa explicabo maiores velit sunt qui earum quasi eaque modi officia 
-            laboriosam, dignissimos numquam incidunt nihil ut facere impedit quia dicta ea debitis reprehenderit. 
-            Provident mollitia quisquam atque ducimus perspiciatis iure repellendus ab distinctio id sapiente incidunt 
-            quis in, voluptatum neque enim vero ipsa vel repellat unde, tempore quaerat nostrum? Blanditiis. reiciendis impedit fuga voluptatum expedita cupiditate perspiciatis accusamus, 
-            ipsa officia, ea maiores laborum autem ipsam minima? Porro mollitia suscipit eaque autem dolores blanditiis 
-            perferendis corrupti ipsam, cumque eveniet ad distinctio quod odio minima rem!"
-        ]
-    ];
-
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]); 
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
 
 
 
