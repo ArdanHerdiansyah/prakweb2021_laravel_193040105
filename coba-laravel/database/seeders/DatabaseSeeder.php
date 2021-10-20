040d686bcfb38c80ca48b4d81e1a18bbdad3fb17
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-10-20 19:33:50 +0700

    Menyelesaikan tugas laravel 8 | 9.Post Category & Eloquent Relationship

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,75 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Ardan Herdiansyah',
+            'email' => 'Ardan@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Riksa Kusumah',
+            'email' => 'Riksa@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]); 
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]); 
+        
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto unde, nobis accusamus facilis sit neque debitis voluptatibus minima ratione at distinctio laborum fuga itaque molestias repellat deserunt quaerat dolorum nulla.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officiis nesciunt nam modi? Corrupti deleniti corporis voluptate, nobis quisquam minus ipsa dolorum deserunt illum natus facere reiciendis obcaecati dolores doloremque porro enim ipsum dignissimos repellat numquam eum eos officiis inventore. Hic voluptatum tempora iusto, quisquam expedita voluptates, ducimus eaque officiis voluptate ab reiciendis recusandae magni sequi ea, consectetur libero quas illo repudiandae nemo necessitatibus earum tenetur quaerat beatae accusamus? Recusandae, id tenetur temporibus ab mollitia nobis enim modi soluta accusamus repellendus quas est repudiandae corporis sit explicabo, illum minus earum a eaque doloribus? Quas sed, perspiciatis autem nobis excepturi esse.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto unde, nobis accusamus facilis sit neque debitis voluptatibus minima ratione at distinctio laborum fuga itaque molestias repellat deserunt quaerat dolorum nulla.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officiis nesciunt nam modi? Corrupti deleniti corporis voluptate, nobis quisquam minus ipsa dolorum deserunt illum natus facere reiciendis obcaecati dolores doloremque porro enim ipsum dignissimos repellat numquam eum eos officiis inventore. Hic voluptatum tempora iusto, quisquam expedita voluptates, ducimus eaque officiis voluptate ab reiciendis recusandae magni sequi ea, consectetur libero quas illo repudiandae nemo necessitatibus earum tenetur quaerat beatae accusamus? Recusandae, id tenetur temporibus ab mollitia nobis enim modi soluta accusamus repellendus quas est repudiandae corporis sit explicabo, illum minus earum a eaque doloribus? Quas sed, perspiciatis autem nobis excepturi esse.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto unde, nobis accusamus facilis sit neque debitis voluptatibus minima ratione at distinctio laborum fuga itaque molestias repellat deserunt quaerat dolorum nulla.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officiis nesciunt nam modi? Corrupti deleniti corporis voluptate, nobis quisquam minus ipsa dolorum deserunt illum natus facere reiciendis obcaecati dolores doloremque porro enim ipsum dignissimos repellat numquam eum eos officiis inventore. Hic voluptatum tempora iusto, quisquam expedita voluptates, ducimus eaque officiis voluptate ab reiciendis recusandae magni sequi ea, consectetur libero quas illo repudiandae nemo necessitatibus earum tenetur quaerat beatae accusamus? Recusandae, id tenetur temporibus ab mollitia nobis enim modi soluta accusamus repellendus quas est repudiandae corporis sit explicabo, illum minus earum a eaque doloribus? Quas sed, perspiciatis autem nobis excepturi esse.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto unde, nobis accusamus facilis sit neque debitis voluptatibus minima ratione at distinctio laborum fuga itaque molestias repellat deserunt quaerat dolorum nulla.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officiis nesciunt nam modi? Corrupti deleniti corporis voluptate, nobis quisquam minus ipsa dolorum deserunt illum natus facere reiciendis obcaecati dolores doloremque porro enim ipsum dignissimos repellat numquam eum eos officiis inventore. Hic voluptatum tempora iusto, quisquam expedita voluptates, ducimus eaque officiis voluptate ab reiciendis recusandae magni sequi ea, consectetur libero quas illo repudiandae nemo necessitatibus earum tenetur quaerat beatae accusamus? Recusandae, id tenetur temporibus ab mollitia nobis enim modi soluta accusamus repellendus quas est repudiandae corporis sit explicabo, illum minus earum a eaque doloribus? Quas sed, perspiciatis autem nobis excepturi esse.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,11 +4,11 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. Ardan Herdiansyah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-        {{-- <h5>{{ $post["author"] }}</</h5> --}}
+    <p>By. <a href="#" class="text-decoration-none"> {{ $post->user->name }}</a>
+        <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        
         {!! $post->body !!} 
-        {{-- tanda seru diatas untuk membuat paragraf --}}
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
 
