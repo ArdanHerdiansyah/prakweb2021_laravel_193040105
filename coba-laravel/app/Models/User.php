5260bc23899d85942d55c8fb255a45429bad4372
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-10-28 14:43:18 +0700

    menyelesaikan video 15 | membuat route & method register, menangani dengan CSRF, membuat validasai dan menangani eror nya, insert data user, mengenskripsi password dan terakhir itu mwmbuat flash message

--- coba-laravel/app/Models/User.php
+++ coba-laravel/app/Models/User.php
@@ -17,11 +17,15 @@ class User extends Authenticatable
      *
      * @var string[]
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
+    // protected $fillable = [
+    //     'name',
+    //     'username',
+    //     'email',
+    //     'password',
+    // ];
+
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -50,7 +50,9 @@ Route::get('/categories', function() {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 
