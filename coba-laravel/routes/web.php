26bf520e21e3c2065a8a659ad4fc475025ad3b3e
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-10-28 12:01:34 +0700

    Menyelesaikan video 13 laravel | Memperbaiki menu active, menambah query(filter dan scope), memperbaiki menu posts, dan menambahkan fitur pagination

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -22,14 +22,14 @@ use App\Models\User;
 Route::get('/', function () {
     return view('home', [
         "title" => "Home",
-        "active" => "Home"
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "active" => "About",
+        "active" => "about",
         "name" => "Ardan Herdiansyah",
         "email" => "ArdanHerdiansyah3@gmail.com",
         "image" => "Al-Quran.jpg"
@@ -37,7 +37,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 //halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
@@ -49,21 +49,8 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => 'categories',
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+
 
 
 
